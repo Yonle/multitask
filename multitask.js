64a106c619a41d1aacb6ad3d99816ac1493e168f
yonle <fledyonle@gmail.com>
2021-08-31 15:00:07 +0700

    Fix typo

--- multitask.js
+++ multitask.js
@@ -1,29 +1,34 @@
 #!/usr/bin/env node
 const multitask = require("./multitask.lib.js");
-let tasks = require(process.argv.slice(2)[0] || process.env.HOME + "/Tasks.json");
-let limit = Number(process.env.MULTITASK_LIMIT) || 4;
-let curTask = 0;
+try {
+	let tasks = require(process.argv.slice(2)[0] || __dirname + "/Tasks.json");
+	let limit = Number(process.env.MULTITASK_LIMIT) || 4;
+	let curTask = 0;
 
-function run() {
-	curTask++
-	if (curTask > limit) return curTask--;
-	if (!tasks.length) return process.exit(0);
-	let task = tasks.shift();
-	if (!task) return;
+	function run() {
+		curTask++
+		if (curTask > limit) return curTask--;
+		if (!tasks.length) return process.exit(0);
+		let task = tasks.shift();
+		if (!task) return;
 	
-	multitask(task[0], task[1], task[2]).then((c) => {
-		curTask--
-		run();
-	});
+		multitask(task[0], task[1], task[2]).then((c) => {
+			curTask--
+			run();
+		});
 
-	if (tasks.length !== 0) run();
+		if (tasks.length !== 0) run();
+	}
+
+	if (tasks.length) {
+		console.log(
+			"Total Tasks: " + tasks.length +
+			"\nEach-Task limit per run: " + limit
+		);
+	}
+
+	run();
+} catch (error) {
+	if (error.code === "MODULE_NOT_FOUND") return console.error("*** Multitask: No task file was found.");
+	console.error(error);
 }
-
-if (tasks.length) {
-	console.log(
-		"Total Tasks: " + tasks.length +
-		"\nEach-Task limit per run: " + limit
-	);
-}
-
-run();
